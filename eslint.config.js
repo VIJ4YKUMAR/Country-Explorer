d70567454c0f32604cb0e3be6e89f69653c481c6
VIJ4YKUMAR <vijayja07@gmail.com>
2024-12-14 18:50:34 +0530

    configure-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import importPlugin from "eslint-plugin-import";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+export default tseslint
+  .config(
+    { ignores: ["dist"] },
+    {
+      extends: [js.configs.recommended, ...tseslint.configs.recommended],
+      files: ["**/*.{js,ts,tsx}"],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+      plugins: {
+        "react-hooks": reactHooks,
+        "react-refresh": reactRefresh,
+        import: importPlugin,
+      },
+      rules: {
+        ...reactHooks.configs.recommended.rules,
+        "react-refresh/only-export-components": [
+          "warn",
+          { allowConstantExport: true },
+        ],
+        "import/no-unresolved": "warn",
+        "no-unused-vars": [
+          "warn",
+          { vars: "all", args: "after-used", ignoreRestSiblings: true },
+        ],
+        "import/no-unused-modules": ["warn", { unusedExports: true }],
+      },
+    }
+  )
+  .concat(eslintPluginPrettier);
